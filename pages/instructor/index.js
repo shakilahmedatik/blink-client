72c2170f8d0b38fee3f56aa2f12bbf4b73b83438
Shakil Ahmed Atik <shakilatik@gmail.com>
2021-10-02 00:15:54 +0600

    import issue fixed

--- pages/instructor/index.js
+++ pages/instructor/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import InstructorRoute from '../../components/routes/InstructorRoute'
+import InstructorRoute from '../../components/Routes/InstructorRoute'
 
 const InstructorIndex = () => {
   return (
