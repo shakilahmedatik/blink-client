37d74e14e7532bab02e208c01426a6d616763709
Shakil Ahmed Atik <shakilatik@gmail.com>
2021-10-02 00:19:16 +0600

    import issue fixed in create page

--- pages/instructor/course/create.js
+++ pages/instructor/course/create.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import InstructorRoute from '../../../components/routes/InstructorRoute'
+import InstructorRoute from '../../../components/Routes/InstructorRoute'
 import CourseCreateForm from '../../../components/Forms/CourseCreateForm'
 
 const CreateCourse = () => {
